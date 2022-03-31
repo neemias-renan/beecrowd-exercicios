657c958e33d4984b54131e59c428a5475fd85266
Neemias Renan <neemiasrenan2020@gmail.com>
2022-03-31 03:01:06 +0000

    Adicionei mais exercício.

---
+++ ex_1002.py
@@ -0,0 +1,13 @@
+# ------
+# A fórmula para calcular a área de uma circunferência é:
+# area = π . raio^2
+# Considerando para este problema que π = 3.14159:
+# Efetue o cálculo da área, elevando o valor de raio ao 
+# quadrado e multiplicando por π.
+# ------
+
+raio = float(input())
+
+area = (3.14159 * (raio**2))
+
+print('A=%.4f'%area)
